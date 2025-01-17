f62a760a59b0f212816d02419673d8a4127d2a70
YHL <yuhsuan.lin@hxldt.com>
2025-01-18 01:40:05 +0800

    0118

---
+++ 檔案複製/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 檔案複製
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = { 1, 4, 8, 5, 9, 7, 2 };
+            //順序不變的條件下，找出總和最大的 group
+            int group = 3;
+
+            //String content = "";
+            //StringBuilder sb = new StringBuilder();
+            //sb.Append(content);
+            //for (int i = 0; i < propertyInfos.Length; i++)
+            //{
+            //    if (i > 0)
+            //        content += ",";
+            //        //sb.Append(",")
+            //    content += propertyInfos[i].GetValue(data);
+            //    //sb.Append(propertyInfos[i].GetValue(data))
+            //}
+
+            //using (var writer = new StreamWriter(filePath, true, Encoding.UTF8))
+            //    writer.WriteLine(sb.ToString());
+        }
+    }
+}
